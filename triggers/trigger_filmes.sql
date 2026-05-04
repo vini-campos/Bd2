92fafa7c209a5653369a06fe4c2ffa9f12bf127d
Vinicius Campos <viniciuscampos131209@gmail.com>
2026-05-04 20:22:49 -0300

    Fix trigger syntax

--- triggers/trigger_filmes.sql
+++ triggers/trigger_filmes.sql
@@ -1,23 +1,23 @@
-create trigger devolver_filme
-on filmes
-after update
-as
-begin
-	declare 
+CREATE TRIGGER devolver_filme
+ON filmes
+AFTER UPDATE
+AS
+BEGIN
+	DECLARE 
 	@status varchar(10),
 	@id_filme int
 
 	SELECT @id_filme = cod_filme, @status = status FROM inserted
 
-	update locacoes set status = @status where cod_filme = @id_filme
+	UPDATE locacoes SET status = @status WHERE cod_filme = @id_filme
 
-	select * from locacoes
-	select * from filmes
+	SELECT * FROM locacoes
+	SELECT * FROM filmes
 
-end
-drop trigger devolver_filme
+END
+DROP TRIGGER devolver_filme
 
-update dbo.filmes
-set status = 'alugado' where cod_filme = 1
+UPDATE dbo.filmes
+SET status = 'alugado' WHERE cod_filme = 1
 
 --arrumar essa merda 
