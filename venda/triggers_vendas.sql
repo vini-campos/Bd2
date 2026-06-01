1d6348e0f34cc512b238e07a02e7ebaad3ccaa3d
vini-campos <viniciuscampos131209@gmail.com>
2026-06-01 19:12:40 -0300

    padronização das informações das tabelas e dos triggers, verificação de saldo, se for negativo ele não aceita.

--- venda/triggers_vendas.sql
+++ venda/triggers_vendas.sql
@@ -1,67 +1,79 @@
-create trigger Ao_inserir_venda
+CREATE TRIGGER Ao_inserir_venda
 ON Venda
-instead of insert
+INSTEAD OF INSERT
 AS
 BEGIN
-	declare @id_venda int,
-			@data_vencimento date
+	DECLARE @id_venda INT,
+			@data_vencimento DATE
 
-	insert into Venda (id_cliente, id_produto, data_venda, quantidade, valor_total, nmr_parcela)
-	select i.id_cliente, i.id_produto, i.data_venda, i.quantidade, p.preco * i.quantidade, i.nmr_parcela
-	from inserted i
-	inner join produto p on p.id = i.id_produto;
+	INSERT INTO Venda (id_cliente, id_produto, data_venda, quantidade, valor_total, nmr_parcela)
+	SELECT i.id_cliente, i.id_produto, i.data_venda, i.quantidade, p.preco * i.quantidade, i.nmr_parcela
+	FROM inserted i
+	INNER JOIN produto p ON p.id = i.id_produto;
 
 	-- pega o id do ultimo indice feito com o insert into
-	set @id_venda = SCOPE_IDENTITY();
+	SET @id_venda = SCOPE_IDENTITY();
 
 	-- insere os valores nos campos com valor calculado
-	INSERT INTO Parcelas (total_parcelas, valor_p_parcela, parcelas_pagas , data_parcela)
-    select i.nmr_parcela, p.preco * i.quantidade / cast(i.nmr_parcela as decimal (10,2)), 1, GETDATE() -- cast foi usado para não cortar a vírgula do numero decimal
-	from inserted i
-	inner join produto p on p.id = i.id_produto;
+	INSERT INTO Parcelas (total_parcelas, valor_p_parcela, parcelas_pagas, data_parcela)
+	SELECT i.nmr_parcela, p.preco * i.quantidade / CAST(i.nmr_parcela AS DECIMAL(10,2)), 1, GETDATE() -- cast foi usado para não cortar a vírgula do numero decimal
+	FROM inserted i
+	INNER JOIN produto p ON p.id = i.id_produto;
 
 	-- vencimento base (1 Mês)
-	select @data_vencimento = DATEADD(month, 1, i.data_venda)
-	from inserted i;
+	SELECT @data_vencimento = DATEADD(MONTH, 1, i.data_venda)
+	FROM inserted i;
 
-	while @data_vencimento in (select dataFeriado from feriados_do_ano)
-		or DATENAME(WEEKDAY, @data_vencimento) in ('Sábado', 'Domingo')--so funciona para dias em portugues (linguagem do sqlserver da escola)
-		or @data_vencimento in (select data_feriado from feriados_fixos)
-	begin
-		set @data_vencimento = DATEADD(day, 1, @data_vencimento);
-	end
+	WHILE @data_vencimento IN (SELECT dataFeriado FROM feriados_do_ano)
+		OR DATENAME(WEEKDAY, @data_vencimento) IN ('Sábado', 'Domingo') --so funciona para dias em portugues (linguagem do sqlserver da escola)
+		OR @data_vencimento IN (SELECT data_feriado FROM feriados_fixos)
+	BEGIN
+		SET @data_vencimento = DATEADD(DAY, 1, @data_vencimento);
+	END
 
-	insert into contas_receber(id_venda, nmr_parcela, data_vencimento, valor_parcela, data_pagamento)
-	select	@id_venda,
+	INSERT INTO contas_receber(id_venda, nmr_parcela, data_vencimento, valor_parcela, data_pagamento)
+	SELECT	@id_venda,
 			i.nmr_parcela,
 			@data_vencimento, -- usa a variavel depois que o dia util foi definido
-			p.preco * i.quantidade / cast(i.nmr_parcela as decimal (10,2)),
+			p.preco * i.quantidade / CAST(i.nmr_parcela AS DECIMAL(10,2)),
 			GETDATE()
-	from inserted i
-	inner join produto p on p.id = i.id_produto;
+	FROM inserted i
+	INNER JOIN produto p ON p.id = i.id_produto;
 
 	-- saida na tabela de movimentos
-	insert into movimento (id_produto, quantidade, tipo)
-	select i.id_produto, i.quantidade, 'S' -- sempre S porque e uma saida
-	from inserted i;
+	INSERT INTO movimento (id_produto, quantidade, tipo)
+	SELECT i.id_produto, i.quantidade, 'S' -- sempre S porque e uma saida
+	FROM inserted i;
 END;
 
-create trigger Atualizar_saldo
-on movimento
-after insert
-as
-begin
-	declare 
-		@e_s int
+CREATE TRIGGER Atualizar_saldo
+ON movimento
+AFTER INSERT
+AS
+BEGIN
+	DECLARE
+		@e_s INT
 
-	select @e_s = case
-		when i.tipo = 'E' then i.quantidade -- entrada, soma
-		when i.tipo = 'S' then - i.quantidade-- saida, subtrai
-	end
-	from inserted i
+	SELECT @e_s = CASE
+		WHEN i.tipo = 'E' THEN i.quantidade -- entrada, soma
+		WHEN i.tipo = 'S' THEN - i.quantidade -- saida, subtrai
+	END
+	FROM inserted i
 
-	update Saldo
-	set Saldo_produto = s.Saldo_produto + @e_s
-	from Saldo s
-	inner join inserted i on s.id_produto = i.id_produto
-end;
+	-- verifica se o saldo do produto e maior do que a quantidade que foi feita a venda do mesmo produto
+	IF EXISTS(
+		SELECT * FROM Saldo s
+		INNER JOIN inserted i on s.id_produto = i.id_produto
+		WHERE i.tipo = 'S' AND s.Saldo_produto + @e_s < 0
+	)
+	BEGIN
+		RAISERROR('Saldo insuficiente para realizar a saída.', 16, 1);
+        ROLLBACK TRANSACTION;
+        RETURN;
+	END
+
+	UPDATE Saldo
+	SET Saldo_produto = s.Saldo_produto + @e_s
+	FROM Saldo s
+	INNER JOIN inserted i ON s.id_produto = i.id_produto
+END;
--- venda/venda.sql
+++ venda/venda.sql
@@ -1,64 +1,65 @@
 CREATE DATABASE financeiro;
 USE financeiro;
+
 CREATE TABLE Cliente(
-	id int identity(1,1) primary key,
+	id INT IDENTITY(1,1) PRIMARY KEY,
 	nome VARCHAR(255) NOT NULL,
 	idade INT NOT NULL
 );
 
-create table Produto(
-	id int identity(1,1) primary key not null,
-	nome varchar(100) not null,
-	preco decimal(10,2) not null,
-	categoria varchar(50) not null,
-	descricao varchar(150)
+CREATE TABLE Produto(
+	id INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+	nome VARCHAR(100) NOT NULL,
+	preco DECIMAL(10,2) NOT NULL,
+	categoria VARCHAR(50) NOT NULL,
+	descricao VARCHAR(150) NOT NULL
 );
 
-create table Venda(
-	id int identity(1,1) primary key not null,
-	id_cliente int foreign key references Cliente(id),
-	id_produto int foreign key references Produto(id),
-	data_venda date not null,
-	quantidade int not null,
-	valor_total float, -- nao precisa do not null porque o trigger ja adiciona
-	nmr_parcela int not null
+CREATE TABLE Venda(
+	id INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+	id_cliente INT FOREIGN KEY REFERENCES Cliente(id) NOT NULL,
+	id_produto INT FOREIGN KEY REFERENCES Produto(id) NOT NULL,
+	data_venda DATE NOT NULL,
+	quantidade INT NOT NULL,
+	valor_total FLOAT, -- nao precisa do not null porque o trigger ja adiciona
+	nmr_parcela INT NOT NULL
 );
 
 CREATE TABLE parcelas(
-	id int identity(1,1) primary key not null,
-	total_parcelas int,
-	valor_p_parcela decimal(10,2),
-	parcelas_pagas int not null,
-	data_parcela date not null,
+	id INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+	total_parcelas INT,
+	valor_p_parcela DECIMAL(10,2),
+	parcelas_pagas INT NOT NULL,
+	data_parcela DATE NOT NULL,
 );
 
-create table Saldo(
-	id_produto int foreign key references Produto(id) not null,
-	Saldo_produto decimal not null
+CREATE TABLE Saldo(
+	id_produto INT FOREIGN KEY REFERENCES Produto(id) NOT NULL,
+	Saldo_produto DECIMAL NOT NULL
 );
 
-create table contas_receber(
-    id_venda int not null,
-    nmr_parcela int not null,
-    data_vencimento date,
-    valor_parcela money not null,
-    data_pagamento date
+CREATE TABLE contas_receber(
+    id_venda INT NOT NULL,
+    nmr_parcela INT NOT NULL,
+    data_vencimento DATE,
+    valor_parcela MONEY NOT NULL,
+    data_pagamento DATE NOT NULL
 );
 
-create table movimento(
-	id int primary key identity(1,1),
-	id_produto int not null,
-	quantidade int not null,
-	tipo char(1) not null check(tipo in ('E', 'S')), -- check é o equivalente ao enum do mysql que vimos em pw
-	data_movimento date default getdate()
+CREATE TABLE movimento(
+	id INT PRIMARY KEY IDENTITY(1,1),
+	id_produto INT NOT NULL,
+	quantidade INT NOT NULL,
+	tipo CHAR(1) NOT NULL CHECK(tipo in ('E', 'S')), -- CHECK é o equivalente ao enum do mysql que vimos em pw
+	data_movimento DATE -- nao precisa de not null
 );
 
-create table feriados_fixos(
-	data_feriado date,
-	descricao varchar(100) not null
+CREATE TABLE feriados_fixos(
+	data_feriado DATE NOT NULL,
+	descricao VARCHAR(100) NOT NULL
 );
 
-create table feriados_do_ano(
-	dataFeriado date primary key,
-	descricao varchar(100) not null
+CREATE TABLE feriados_do_ano(
+	dataFeriado DATE PRIMARY KEY NOT NULL,
+	descricao VARCHAR(100) NOT NULL
 );
